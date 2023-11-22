86ea3215e847e03097275950c727e7502c56d233
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-22 10:49:58 -0400

    Primeros pasos en formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  // Crear formulario reactivo
+  // * (valor inicial, validadores síncronos, validadores asíncronos)
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  // * Mismo formulario con FormBuilder
+
+  public myForm: FormGroup = this.fb.group({
+    name: [''], 
+    price: [0],
+    inStorage: [0],
+  })
+
+  constructor( private fb: FormBuilder) {}
+
+  // Posteo de la información
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 
 }
