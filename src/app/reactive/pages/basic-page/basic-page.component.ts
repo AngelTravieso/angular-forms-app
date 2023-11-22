9a106b5aa336d8cce6429177fa1a461fbfe7ec0d
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-22 13:59:12 -0400

    Reestablecer y establecer valor al formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // Crear formulario reactivo
   // * (valor inicial, validadores síncronos, validadores asíncronos)
@@ -26,6 +32,15 @@ export class BasicPageComponent {
 
   constructor( private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    /**
+     * Esto serviría para inicializar el formulario con unos valores ya cargados
+     * por ejemplo, valores que pudiesemos recibir de un servicio del backend,
+     * una vez recibido se inicializa el formulario con estos valores
+     */
+    this.myForm.reset(rtx5090);
+  }
+
   // Posteo de la información
   onSave(): void {
 
@@ -33,6 +48,16 @@ export class BasicPageComponent {
     if(this.myForm.invalid) return;
 
     console.log(this.myForm.value);
+
+    // Restablecer formulario a su valor pristine y untouched (inicial)
+    this.myForm.reset();
+
+    // Esto hace un reset del formulario con los valores dados
+    // this.myForm.reset({
+    //   price: 10,
+    //   inStorage: 0,
+    // });
+
   }
 
 }
